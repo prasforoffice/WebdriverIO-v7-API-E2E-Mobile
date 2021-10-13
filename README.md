529d199a2e12d4c6f8d7388d77801fe0ffd9b048
PKN <pkn@a.com>
2021-10-13 13:53:10 +0530

    Readme updated.renamed the script names in pacakge json

--- README.md
+++ README.md
@@ -1,1 +1,66 @@
 # assignmentsAB
+
+This is an E2E integration tests project for testing ParaBank application (https://parabank.parasoft.com/parabank/index.htm).In this project user interacts with Parabank web application and creates new accounts, carries out Bill payment transactions and perfroms relevant validations across screens.
+
+These tests are developed in Javascript with [WebDriverIO V7](http://webdriver.io/) and [Mocha](https://mochajs.org/)
+
+
+## Features
+
+-   Javascript
+-   Page Object Design Pattern
+-   [Applitools Eyes](https://info.applitools.com/ucY76)
+-   Spec reporter
+-   Allure report (screenshots on failure as well passed steps)
+
+
+## Requirements
+
+-   node >= 14.x.x - [how to install Node](https://nodejs.org/en/download/)
+-   npm >= 6.14.x - [how to install NPM](https://www.npmjs.com/get-npm)
+
+## Config Files
+
+ WebdriverIO configuration is fully customizable, and different functions can be invoked before, during and after each test or test suite.  Config files can be found in the `./test/config/` directory and end with `*.conf.js`.  These can be called via the the cli.
+
+## Getting Started
+
+### Install the dependencies:
+
+```bash
+npm install
+```
+
+
+### Run e2e tests:
+
+```bash
+npm run test:e2e
+```
+
+### Run visual regression tests using Applitools:
+
+```bash
+npm run tests:vr
+```
+
+## Reports
+
+### Spec
+
+Test reporter, that prints detailed results to console.
+
+### Allure
+
+Run this command to generate the allure report in the directory `./test/reports/allure-report`:
+
+```bash
+npm run report:generate
+```
+
+You can run this command to start a server on your machine and open the allure report on the browser:
+
+```bash
+npm run report:open
+```
+
---
+++ test/specs/e2e_RegressionTest.js
@@ -0,0 +1,106 @@
+const LoginPage = require('../pageobjects/loginPage');
+const AccountServicesPage = require('../pageobjects/accountServicesPage');
+const OpenNewAccountPage = require('../pageobjects/openNewAccountPage');
+const AccountDetailsPage = require('../pageobjects/accountDetailsPage');
+const AccountsOverviewPage = require('../pageobjects/accountsOverviewPage');
+const BillPayPage = require('../pageobjects/billPayPage');
+const Labels = require('../constants/pageLabels.js');
+const Yaml = require('js-yaml');
+const Fs = require('fs'); 
+
+
+const TestData = Yaml.load(Fs.readFileSync('./test/testData/userInputs.yml')); //To read the test data yaml file
+
+describe('Verification of opening two new accounts for both account types - CHECKING and SAVINGS', () => {
+
+    var firstAccountNumber, secondAccountNumber,firstAccountBalance,secondAccountBalance;
+    var billPaymentData, secondAccountData, firstAccountData;
+
+    before(async () => {
+        await LoginPage.open();
+    });
+
+    it('Verify user can login with valid credentials', async () => {
+        let loginData = TestData['LoginData'];
+        await LoginPage.open();
+        await LoginPage.login(loginData.username, loginData.password);
+        await expect(AccountServicesPage.welcomeText).toBeExisting();
+    });
+
+    it('Verify user sees a welcome message with a full name in Account Services section', async () => {
+        await expect(AccountServicesPage.accountServicesLabel).toBeExisting();
+        await expect(AccountServicesPage.welcomeText).toHaveTextContaining(Labels.WELCOME_TEXT);
+    });
+
+    /* Section for creating 1st account */
+
+    it('Verify user is able to launch,fill and submit New Account form to create 1st account', async () => {
+        firstAccountData = TestData['FirstAccountData'];
+        await AccountServicesPage.clickOpenNewAccountLink();
+        await expect(AccountServicesPage.openNewAccountFormLabel).toBeExisting();
+        await OpenNewAccountPage.fillNewAccountOpeningForm(firstAccountData);
+        await OpenNewAccountPage.validateSucessMessages();
+        firstAccountNumber = await OpenNewAccountPage.getNewAccountNumber()
+    });
+
+    it('Verify user is able to launch & validate Account Details page of the 1st account ', async () => {
+        await OpenNewAccountPage.clickNewAccountNumber();
+        await AccountDetailsPage.validateNewAccountDetails(firstAccountNumber,firstAccountData.type);
+        firstAccountBalance = await AccountDetailsPage.getAccountBalance();
+        firstAccountAvlBalance = await AccountDetailsPage.getAvailableBalance();
+
+
+    });
+
+    /*Section for creating 2nd account*/
+
+    it('Verify user is able to launch,fill and submit New Account form to create 2nd account', async () => {
+        secondAccountData = TestData['SecondAccountData'];
+        await AccountServicesPage.clickOpenNewAccountLink();
+        await expect(AccountServicesPage.openNewAccountFormLabel).toBeExisting();
+        await OpenNewAccountPage.fillNewAccountOpeningForm(secondAccountData);
+        await OpenNewAccountPage.validateSucessMessages();
+        secondAccountNumber = await OpenNewAccountPage.getNewAccountNumber()
+
+    });
+
+    it('Verify user is able to launch & validate Account Details page of the 2nd account ', async () => {
+        await OpenNewAccountPage.clickNewAccountNumber();
+        await AccountDetailsPage.validateNewAccountDetails(secondAccountNumber,secondAccountData.type);
+        secondAccountBalance = await AccountDetailsPage.getAccountBalance();
+        secondAccountAvlBalance = await AccountDetailsPage.getAvailableBalance();
+    });
+
+    it('Verify user can launch and fill Bill Pay form', async () => {
+        billPaymentData = TestData['BillPaymentData'];
+        await AccountServicesPage.clickBillPayLink();
+        await BillPayPage.fillBillPaymentForm(billPaymentData, secondAccountNumber, firstAccountNumber);
+    });
+
+    it('Validate the details on Bill Pay confirmation screen are correct', async () => {
+        await BillPayPage.validateBillPayConfirmationDetails(billPaymentData.name, billPaymentData.amount, firstAccountNumber);
+    });
+
+
+    it('Validate the Bill payment transactions across two new accounts', async () => {
+         /* First account verification */
+        await AccountServicesPage.clickAccountsOverviewLink();
+        await AccountsOverviewPage.openAccountFromOverviewPage(secondAccountNumber);
+        await AccountDetailsPage.checkIfAccountDetailsScreenLoaded();
+        await AccountDetailsPage.validateAccountBalance(secondAccountBalance,secondAccountAvlBalance);
+
+        /*  Second account verification */
+        await AccountServicesPage.clickAccountsOverviewLink();
+        await AccountsOverviewPage.openAccountFromOverviewPage(firstAccountNumber);
+        await AccountDetailsPage.checkIfAccountDetailsScreenLoaded();
+        var currentBalance = parseFloat(firstAccountBalance.replace(/[^\d\.]/, '')) - 200;
+        currentBalance = AccountDetailsPage.convertNumberToCurrencyAmount(currentBalance);
+        await AccountDetailsPage.validateAccountBalance(currentBalance,AccountDetailsPage.convertNumberToCurrencyAmount("0"));
+
+    });
+
+    
+
+
+
+});
